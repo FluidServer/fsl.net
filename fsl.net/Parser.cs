bc6c8bbe2d0893c73971a223de03df0470c464ac
fgclue <lost@biitle.nl>
2025-02-09 15:13:14 -0300

    Fix bugs

--- fsl.net/Parser.cs
+++ fsl.net/Parser.cs
@@ -10,25 +10,25 @@ namespace fsl.net
     {
         public Dictionary<string, string> Parse(string input)
         {
+            if(input == "" || input == null) { throw new Exception("Input cannot be null"); }
             string[] split = input.Split('\n');
 
-            string fslVersion = split[0].Replace(@"\s+", "").ToString().Split(',')[1].Split('.')[0];
+            string fslVersion = split[0].Replace(" ", "").ToString().Split(',')[1].Split('.')[0];
 
             var settings = new Dictionary<string, string>
             {
                 { "Version", fslVersion }
             };
 
-            settings.Add("Version", fslVersion);
-
-            for (int i = 0; i < split.Length; i++)
+            for (int i = 0; i < split.Length - 1; i++)
             {
                 var pairs = split[i].Split('=');
 
                 if(split[i].StartsWith(";") || split[i].StartsWith(":")) { continue; }
-                if(pairs.Length < 2) { continue; }
-                if(pairs.Length > 2) { continue; }
-                settings.Add(pairs[0], pairs[1]);
+                if(pairs.Length != 2) { continue; }
+                try {
+                    settings.Add(pairs[0], pairs[1]);
+                } catch { continue; }
             }
 
             return settings; 
